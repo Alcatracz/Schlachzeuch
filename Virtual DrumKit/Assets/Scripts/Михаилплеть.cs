d875a5e80f3d9878a0ad3c6d02e73d2d97421238
Arne B <arno7@web.de>
2017-11-21 21:16:22 +0100

    Play_Audio_on_CD_with_CDplayer
    123

--- Virtual DrumKit/Assets/Scripts/Михаилплеть.cs
+++ Virtual DrumKit/Assets/Scripts/Михаилплеть.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
+[RequireComponent(typeof(Interactable))]
 public class Михаилплеть : MonoBehaviour {
+
+	public GameObject goforposition;
+	private Hand hand;
+	private bool atached;
+	private GameObject cd;
+
 
 	// Use this for initialization
 	void Start () {
@@ -11,20 +19,23 @@ public class Михаилплеть : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (atached&&hand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip)) {
+			cd.transform.position = goforposition.transform.position;
+			cd.transform.rotation = goforposition.transform.rotation;
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "CD") {
 			other.gameObject.GetComponent<AudioSource> ().Play();
-			other.gameObject.GetComponent<Rigidbody>().useGravity=false;
+			atached = true;
 		}
 	}
 
-	void onTriggerExit(Collider other){
+	void OnTriggerExit(Collider other){
 		if (other.gameObject.tag == "CD") {
 			other.gameObject.GetComponent<AudioSource> ().Pause();
-			other.gameObject.GetComponent<Rigidbody>().useGravity=true;
+			atached = false;
 		}
 	}
 
