39aa58fd064122648f7f741bf822623eabad6366
Alcatracz <jan.ole.clausen78@googlemail.com>
2017-11-22 02:17:45 +0100

    3d sound

--- Virtual DrumKit/Assets/Scripts/Михаилплеть.cs
+++ Virtual DrumKit/Assets/Scripts/Михаилплеть.cs
@@ -1,43 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR.InteractionSystem;
 
-[RequireComponent(typeof(Interactable))]
-public class Михаилплеть : MonoBehaviour {
-
-	public GameObject goforposition;
-	private Hand hand;
-	private bool atached;
-	private GameObject cd;
+public class Михаилплеть : MonoBehaviour
+{
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (atached&&hand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip)) {
-			cd.transform.position = goforposition.transform.position;
-			cd.transform.rotation = goforposition.transform.rotation;
-		}
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CD")
+        {
+            other.gameObject.GetComponent<AudioSource>().Play();
+          
+        }
+    }
 
-	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.tag == "CD") {
-			other.gameObject.GetComponent<AudioSource> ().Play();
-			atached = true;
-		}
-	}
-
-	void OnTriggerExit(Collider other){
-		if (other.gameObject.tag == "CD") {
-			other.gameObject.GetComponent<AudioSource> ().Pause();
-			atached = false;
-		}
-	}
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "CD")
+        {
+            other.gameObject.GetComponent<AudioSource>().Pause();
+           
+        }
+    }
 
 
 }
