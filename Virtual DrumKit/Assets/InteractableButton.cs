581f42349dbf4ace4690a02905754973f129d786
Alcatracz <jan.ole.clausen78@googlemail.com>
2017-11-29 16:55:14 +0100

    asf

---
+++ Virtual DrumKit/Assets/InteractableButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Events;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+public class InteractableButton : MonoBehaviour {
+
+    public UnityEvent onButtonPress;
+}
