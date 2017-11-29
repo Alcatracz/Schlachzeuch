2744cda1c1a7fc1a0fbb352fbee17f3295e966dc
Arne B <arno7@web.de>
2017-11-29 19:27:11 +0100

    PitchHandler_v0.000001
    default

---
+++ Virtual DrumKit/Assets/Scripts/PitchHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitchHandler : MonoBehaviour {
+
+	public Vector3 velocity;
+	private float magnitude;
+	private AudioSource audiosource;
+
+	// Use this for initialization
+	void Start () {
+		audiosource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		magnitude = velocity.magnitude;
+		audiosource.volume = 1 * magnitude;
+	}
+}
