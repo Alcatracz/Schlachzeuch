f247664c2bd7e2ad0bf6b40a098739b65f2e0e49
Alcatracz <jan.ole.clausen78@googlemail.com>
2017-11-29 17:19:02 +0100

    InteractableButton added

--- Virtual DrumKit/Assets/InteractableButton.cs
+++ Virtual DrumKit/Assets/InteractableButton.cs
@@ -9,4 +9,26 @@ using Valve.VR.InteractionSystem;
 public class InteractableButton : MonoBehaviour {
 
     public UnityEvent onButtonPress;
+
+    void OnHandHoverBegin(Hand hand)
+    {
+        ControllerButtonHints.ShowTextHint(hand, Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger, "Press F to pay Respect");
+    }
+
+    void OnHandHoverEnd(Hand hand)
+    {
+        ControllerButtonHints.HideTextHint(hand, Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        //Trigger got pressed
+        if (hand.GetStandardInteractionButtonDown())
+        {
+            onButtonPress.Invoke();
+            ControllerButtonHints.HideTextHint(hand, Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
+        }
+    }
+
+
 }
---
+++ Virtual DrumKit/Assets/Tester.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour {
+
+public void ping()
+    {
+        Debug.Log("Ping");
+        print("ping");
+    }
+}
