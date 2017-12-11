7fcd28c6400f6514e4a5e11f4f856727e46bcf8c
Arne B <arno7@web.de>
2017-12-11 13:20:24 +0100

    restrt
    <>

---
+++ Virtual DrumKit/Assets/Scripts/RestartScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartScene : MonoBehaviour {
+	//public int scene;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void resetscene(int scene){
+		Application.LoadLevel(scene);
+	}
+}
