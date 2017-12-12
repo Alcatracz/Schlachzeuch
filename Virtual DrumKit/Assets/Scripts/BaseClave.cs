442757a27afd882e01381da4ae0576da989b9fb6
Alcatracz <jan.ole.clausen78@googlemail.com>
2017-12-12 20:28:35 +0100

    Klanghölzer

---
+++ Virtual DrumKit/Assets/Scripts/BaseClave.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class BaseClave : MonoBehaviour {
+
+    private float hitVelocity = 0.0f;
+    private AudioSource audioSource;
+    float standardvolume = 1.0f;
+    private Hand hand;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        //standardvolume = audioSource.volume;
+    }
+
+    void OnAttachedToHand(Hand hand)
+    {
+        this.hand = hand;
+        print("Attached");
+    }
+
+    void OnDetachedFromHand(Hand hand)
+    {
+        print("Detached");
+        this.hand = null;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Claves")
+        {
+            other.gameObject.GetComponent<HitClave>().Collided();
+            hitVelocity = other.gameObject.GetComponent<HitClave>().GetStickVelocity();
+            print("HitVelocity: "+hitVelocity);
+            print("StandardVolume: "+standardvolume);
+            audioSource.volume = standardvolume * hitVelocity;
+            print("Volume: "+audioSource.volume);
+            audioSource.Play();
+            hand.controller.TriggerHapticPulse(400);
+            hand.otherHand.controller.TriggerHapticPulse(400);
+        }
+
+    }
+}
---
+++ Virtual DrumKit/Assets/Scripts/HitClave.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class HitClave : MonoBehaviour {
+
+    private float stickvelocity = 1.0f;
+    private Vector3 velocity = Vector3.zero;
+    private VelocityEstimator ve;
+    
+    void Start () {
+        ve = GetComponent<VelocityEstimator>();
+        ve.BeginEstimatingVelocity();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Collided()
+    {
+        print("Collideed");
+        ve.FinishEstimatingVelocity();
+        velocity = ve.GetVelocityEstimate();
+        stickvelocity = velocity.magnitude;
+        if (stickvelocity < 0)
+        {
+            stickvelocity = stickvelocity * -1;
+        }
+        ve.BeginEstimatingVelocity();
+    }
+
+    public float GetStickVelocity()
+    {
+        print("GetStickVelocity: "+stickvelocity);
+        return this.stickvelocity;
+    }
+}
