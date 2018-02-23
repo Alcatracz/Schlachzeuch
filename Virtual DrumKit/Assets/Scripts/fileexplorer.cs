07c1f176e91ce6e18b9aa88ffbe1431faebfc2c7
Arne B <arno7@web.de>
2018-02-23 20:24:58 +0100

    k
    k

---
+++ Virtual DrumKit/Assets/Scripts/fileexplorer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class fileexplorer : MonoBehaviour {
+
+	string path;
+
+	public void OpenExplorer(){
+		
+         path = EditorUtility.OpenFolderPanel("Choose", "", "");
+
+}
+}
