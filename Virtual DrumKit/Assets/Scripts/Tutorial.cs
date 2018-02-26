074c025dd64081e566b33cb0b12e88bd2b9d5024
Alcatracz <jan.ole.clausen78@googlemail.com>
2018-02-26 23:08:34 +0100

    sudoku
    <>

--- Virtual DrumKit/Assets/Scripts/Tutorial.cs
+++ Virtual DrumKit/Assets/Scripts/Tutorial.cs
@@ -19,17 +19,17 @@ public class Tutorial : MonoBehaviour {
 		else if (other.gameObject.name == "Aufnahmelocation")
 		{
 			popup.SetActive (true);
-			popuptext.text="Drücke den roten Knopf auf dem Mikrophone um eine Aufnahme zu beginnen und erneut um selbige zu stoppen. Hiernach solltest du dich zur Position 'Aufnahmeabspielen' teleportieren";
+			popuptext.text="Drücke den roten Knopf auf dem Mikrofon, um eine Aufnahme zu beginnen und erneut um selbige zu stoppen. Hiernach solltest du dich zur Position 'Aufnahmeabspielen' teleportieren";
 		}
 		else if (other.gameObject.name == "Aufnahmeabspielenlocation")
 		{
 			popup.SetActive (true);
-			popuptext.text="Wen du eine Aufnahme abgeschlossen hast erscheint auf dem Tisch vor dir eine CD die du abspielen kannst, indem du diese auf den Recorder legst";
+			popuptext.text="Wen du eine Aufnahme abgeschlossen hast, erscheint auf dem Tisch vor dir eine CD, die du abspielen kannst, indem du diese auf den Recorder legst";
 		}
 		else if (other.gameObject.name == "Perkursionsinstrumentelocation"||other.gameObject.name == "Perkursionsinstrumentelocation2")
 		{
 			popup.SetActive (true);
-			popuptext.text="Hier gibt es noch einige weitere simple Instrumente mit denen du spielen kannst nimm sie einfach auf.";
+			popuptext.text="Hier gibt es noch einige weitere simple Instrumente mit denen du spielen kannst. Nimm sie einfach auf.";
 		}
 	}
 
